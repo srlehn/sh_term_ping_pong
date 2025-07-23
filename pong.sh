e427aa4d4bc74f523df6cfa4754663e2d4f9fb8b
Simon Lehn <48837958+srlehn@users.noreply.github.com>
2025-07-23 22:35:25 +0200

    comment pong.sh, ...

--- pong.sh
+++ pong.sh
@@ -5,6 +5,25 @@
 # check if sourced
 [ "$0" != "${BASH_SOURCE[0]}" ] || { printf %s\\n 'source this script' ". \"$0\"" >&2; exit 1; }
 
+# disable echo
 stty -echo
-alias t='((i)) && { i=0; a=i; } || { i=1; a=o; }; echo "p${a}ng"; bind "\"\\e[0n\": \"t\\n\""; printf "\\e[5n"'
-t
+# reenable echo on Ctr-C
+trap 'stty echo; trap - INT; (exit 130)' INT
+
+# bind key sequence to the function name with return (this executes the function)
+# to the terminal escape sequence response.
+bind "\"\\e[0n\": \"main\\n\""
+
+main(){
+  # alternate
+  ((i)) && { i=0; a=i; } || { i=1; a=o; }
+  # actual output
+  echo "p${a}ng"
+  # trigger the terminal to send an escape sequence response
+  printf "\\e[5n"
+  # with a small pause, the shell terminal interaction becomes interruptable with Ctr-C
+  sleep .15
+}
+
+# start it
+main
